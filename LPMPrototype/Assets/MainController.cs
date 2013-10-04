fac8e0a8830c0a735059500573878a38e7002343
Looksky <robin.bosma@hotmail.com>
2013-10-04 12:30:29 +0200

    LPM update

--- LPMPrototype/Assets/MainController.cs
+++ LPMPrototype/Assets/MainController.cs
@@ -16,7 +16,7 @@ public class MainController : MonoBehaviour
 {
 
     private Room m_currentRoom = Room.Caretaker;
-    List<TextController> m_textControllers = new List<TextController>();
+    private List<TextController> m_textControllers = new List<TextController>();
 
     private List<Texture2D> m_textures = new List<Texture2D>();
     private ImageController m_imageController;
@@ -25,31 +25,32 @@ public class MainController : MonoBehaviour
     public Dictionary<Room, GameObject[]> m_Objects = new Dictionary<Room,GameObject[]>();
     public GameObject[] Scenes;
 
-    Vector2 m_scrollPos;
+    private Vector2 m_scrollPos;
     public static float m_boxHeight = 250.0f;
 
 
     void Start () 
     {
         m_imageController = transform.GetChild(0).GetComponent<ImageController>();
+		m_textControllers.Add((TextController)(new TextControllerMenu(this)));
         Texture2D tex = (Texture2D)Resources.Load("room_0");
         m_textures.Add(tex);
 
         for (Room r = Room.Caretaker; r < Room.RoomsCount; r++)
         {
             TextAsset questions = (TextAsset)Resources.Load("questions_" + ((int)r));
-            TextAsset answears = (TextAsset)Resources.Load("answears_" + ((int)r));
+            TextAsset answer = (TextAsset)Resources.Load("answears_" + ((int)r));
             TextAsset objects = (TextAsset)Resources.Load("objects_" + ((int)r));
             
             tex = (Texture2D)Resources.Load("room_" + ((int)r));
 
-            m_textControllers.Add(new TextController(objects, questions, answears, Room.Biology));
+            m_textControllers.Add(new TextController(objects, questions, answer, Room.Biology));
             m_textures.Add(tex);
         }
         if (Scenes.Length > 0)
         {
             if (Scenes.Length != (int)Room.RoomsCount)
-                Debug.Log("ERROR: LESS SCENES THAN ROOMS");
+                Debug.LogError("ERROR: LESS SCENES THAN ROOMS");
 
             for (Room r = Room.Menu; r < Room.RoomsCount; r++)
             {
@@ -66,15 +67,16 @@ public class MainController : MonoBehaviour
     {
         if (m_currentTextController != null)
             m_currentTextController.ShowDialog = false;
-
+		
         m_imageController.SetImage(m_textures[(int)r]);
         
-        if ((int)r - 1 >= 0)
+        if ((int)r >= 0)
         {
-            m_currentTextController = m_textControllers[(int)r - 1];
+            m_currentTextController = m_textControllers[(int)r];
             m_currentTextController.ShowDialog = true;
         }
     }
+	
     public void Back()
     {
         if(m_currentTextController != null)
@@ -83,12 +85,15 @@ public class MainController : MonoBehaviour
         if (m_imageController.isZoomed)
             m_imageController.Zoom(false, Vector2.zero);
         else
+		{
             SelectRoom(Room.Menu);
+		}
     }
+	
 	void Update () 
     {
         if (Input.GetMouseButtonDown(0))
-            m_currentTextController.NextAnswearLine();
+            m_currentTextController.NextAnswerLine();
 	}
 
     void OnGUI()
@@ -102,21 +107,22 @@ public class MainController : MonoBehaviour
             m_scrollPos = GUILayout.BeginScrollView(m_scrollPos, false, false);
             switch (m_currentTextController.m_displayType)
             {
-                case DisplayType.Answear:
-                    GUILayout.Label(m_currentTextController.m_currentQuestion.answearText[
-                                                        m_currentTextController.m_currentAnswearLine]);
+                case DisplayType.Answer:
+                    GUILayout.Label(m_currentTextController.m_currentQuestion.answerText[
+                                                        m_currentTextController.m_currentanswerLine]);
                     break;
                 case DisplayType.Questions:
-                    int i = 0;
-                    foreach (var q in m_currentTextController.m_questions)
+					int i = 0;
+					for(i = 0; i < m_currentTextController.m_questions.Count; i++)
                     {
-                        string txt = (++i) + "." + q.questionText;
+						Question q = m_currentTextController.m_questions[i];
+                        string txt = (i) + "." + q.questionText;
                         if (GUILayout.Button(txt))
-                            m_currentTextController.Answear(i - 1);
+                            m_currentTextController.Answer(i);
                     }
-                    string text = (++i) + ".Back";
+                    string text = (i++) + ".Back";
                     if (GUILayout.Button(text)) Back(); break;
-                case DisplayType.ObjectDesctiption:
+                case DisplayType.ObjectDescription:
                     GUILayout.Label(m_currentTextController.m_currentObject);
                     break;
 
