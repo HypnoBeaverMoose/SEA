e09be485b11191addaccbfd10327c5d6a1a161e9
HypnoBeaverMoose <y.atanasoff@gmail.com>
2013-12-09 11:41:28 +0100

    asdasdasd

---
+++ PlantGeneratorGUI/PlantGeneratorGUI/android/gen/org/kde/necessitas/ministro/IMinistroCallback.java.d
@@ -0,0 +1,3 @@
+G:\SEA\PlantGeneratorGUI\PlantGeneratorGUI\android\gen\org\kde\necessitas\ministro\IMinistroCallback.java: \
+  G:\SEA\PlantGeneratorGUI\PlantGeneratorGUI\android\src\org\kde\necessitas\ministro\IMinistroCallback.aidl 
+
